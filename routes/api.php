e8f1b7ce213296e2f3bfcc0da7a5542253342cd9
Audace SANGANO <69398510+sano20@users.noreply.github.com>
2022-01-19 09:17:44 +0200

    .

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EventController;
+use App\Http\Middleware\CheckUser;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,9 @@ use App\Http\Controllers\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::post('login',[AuthController::class,"login"]);
-Route::post('logout',[AuthController::class,"logout"]);
+
+Route::middleware([CheckUser::class])->group(function () {
+    Route::post('registerEvent',[EventController::class,"createEvent"]);
+    Route::post('logout',[AuthController::class,"logout"]);
+});
