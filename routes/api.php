52a717d0fdcc999a39143f4f74a05d24a420c2e6
Audace SANGANO <69398510+sano20@users.noreply.github.com>
2022-01-24 08:27:10 +0200

    final

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,45 @@ use App\Http\Middleware\CheckUser;
 */
 Route::post('login',[AuthController::class,"login"]);
 
+<<<<<<< Updated upstream
 Route::middleware([CheckUser::class])->group(function () {
     Route::post('registerEvent',[EventController::class,"createEvent"]);
     Route::post('logout',[AuthController::class,"logout"]);
 });
+=======
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:api','jwt.verify','active')->group(function(){
+    Route::put('update-user-info',[UserApiController::class,'updateUserInfo']);
+    Route::put('update-password',[UserApiController::class,'updatePassword']);
+    Route::get('logout', [UserApiController::class,'logout']);
+    Route::get('user', [UserApiController::class,'getAuthUser']);
+
+    // only admin routes
+    Route::middleware('admin')->prefix('admin')->group(function(){
+        Route::get('users', [UserApiController::class,'getUsers']);
+        Route::get('clients', [UserApiController::class,'getClients']);
+        Route::get('users/{user}/view', [UserApiController::class,'getUser']);
+        Route::get('clients/{user}/view', [UserApiController::class,'getClient']);
+        Route::put('users/{user}/block', [UserApiController::class,'blockUnblock']);
+        Route::post('register-client',[UserApiController::class,'registerClient']);
+        Route::post('register-admin',[UserApiController::class,'registerAdmin']);
+    });
+
+    // only client routes
+    Route::middleware('client')->prefix('dashboard')->group(function(){
+        Route::post('register-event',[EventsController::class,'store']);
+    });
+
+    // only normal users routes
+    Route::middleware('user')->group(function(){
+        
+    });
+});
+
+Route::post('register',[UserApiController::class,'register']);
+Route::post('login', [UserApiController::class,'login']); 
+
+>>>>>>> Stashed changes
